aa8aab54fcdcf4a4a4a29af407a22c66663822de
Pedro Fernandez <pedro@pandadesigners.com>
2025-05-09 19:12:47 -0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex mini-h-screen flex-col items-center justify-center p-24 text-indigo-200">
+      <span>Hola mundo</span>
+    </div>
+  );
+}
